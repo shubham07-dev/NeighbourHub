aa452349a260189ea2909aa6c73320c1b8987006
shashankrai-cse <iamshashankrai@gmail.com>
2026-04-08 11:32:50 +0530

    BY shashank

---
+++ backend/models/Job.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  provider: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Provider',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'ongoing', 'completed', 'rejected'],
+    default: 'pending',
+  },
+  reviews: {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+    }
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Job', jobSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+  },
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
